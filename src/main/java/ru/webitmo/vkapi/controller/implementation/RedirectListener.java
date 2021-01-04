52b0ff5dcfb68befedb68d87a69bfd6e13d381d5
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2021-01-04 05:40:06 +0300

    added rest endpoint for getting access code

---
+++ src/main/java/ru/webitmo/vkapi/controller/implementation/RedirectListener.java
@@ -0,0 +1,22 @@
+package ru.webitmo.vkapi.controller.implementation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.webitmo.vkapi.service.controller.MainController;
+
+@Slf4j
+@RestController
+@RequestMapping
+public class RedirectListener {
+    private final MainController mainController;
+
+    public RedirectListener(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    @GetMapping("/authorize")
+    public @ResponseBody String getCode(@RequestParam String code) {
+        log.info("code taken by redirect");
+        return mainController.getUserInfo(code);
+    }
+}
