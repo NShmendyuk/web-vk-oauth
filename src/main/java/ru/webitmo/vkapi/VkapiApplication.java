fdfd0d8de070a7e3c115c9dfcdcceb5d511a47cf
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2021-01-04 05:16:33 +0300

    created spring application

---
+++ src/main/java/ru/webitmo/vkapi/VkapiApplication.java
@@ -0,0 +1,13 @@
+package ru.webitmo.vkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkapiApplication.class, args);
+    }
+
+}
