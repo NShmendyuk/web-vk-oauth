1d1d7c2beb24f43d1cdd6f791706e49dc41f98bc
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2021-01-05 17:43:05 +0300

    added checking for vk app credentials

--- src/main/java/ru/webitmo/vkapi/Starter.java
+++ src/main/java/ru/webitmo/vkapi/Starter.java
@@ -2,6 +2,7 @@ package ru.webitmo.vkapi;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.webitmo.vkapi.controller.AuthController;
 
@@ -9,9 +10,15 @@ import ru.webitmo.vkapi.controller.AuthController;
 @RequiredArgsConstructor
 public class Starter {
     private final AuthController authController;
+    @Value("${vk.client_id}")
+    String client_id;
+    @Value("${vk.client_secret}")
+    String client_secret;
 
     @Autowired
     private void showVkUserName() {
+        if (client_id.equals("") || client_secret.equals(""))
+            System.out.println("Please, add vk credentials into application.properties");
         authController.requestAccessCode();
     }
 }
