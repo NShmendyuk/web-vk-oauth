ce33723d86a8893d9b78b81f8f6764aafefed897
Nshmendyuk <Nshmendyuk.ru@yandex.ru>
2021-01-05 04:44:25 +0300

    refactored annotation

--- src/main/java/ru/webitmo/vkapi/controller/implementation/RedirectListener.java
+++ src/main/java/ru/webitmo/vkapi/controller/implementation/RedirectListener.java
@@ -1,21 +1,18 @@
 package ru.webitmo.vkapi.controller.implementation;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.webitmo.vkapi.service.controller.MainController;
 
 @Slf4j
 @RestController
-@RequestMapping
+@RequiredArgsConstructor
 public class RedirectListener {
     private final MainController mainController;
 
-    public RedirectListener(MainController mainController) {
-        this.mainController = mainController;
-    }
-
     @GetMapping("/authorize")
-    public @ResponseBody String getUserName(@RequestParam String code) {
+    public String getUserName(@RequestParam String code) {
         log.info("code taken by redirect");
         return mainController.getUserInfo(code);
     }
